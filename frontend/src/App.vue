b078d0947e98a93af7072da1965d0fa61d0a1783
Doan Le Manh Tung <tungdlm@cyradar.com>
2020-09-07 19:12:31 +0700

    Init Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">   -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <Test/> -->
+    <!-- <Example1/> -->
+    <Example2/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+//import Test from './components/Test.vue'
+//import Example1 from './components/Example1.vue'
+import Example2 from './components/Example2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    //Test
+    //Example1
+    Example2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="test">
+		{{ info }}
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+	name: 'Test',
+	data(){
+		return{
+			info: null
+		}	
+	},
+	mounted(){
+		axios
+			.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+			.then(response => (this.info = response.data.bpi))
+	}
+}
+</script>
